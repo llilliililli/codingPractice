a5f6929f9d98819804a63b8322f972984f16ba31
llilliililli <wnsrn403@naver.com>
2022-03-25 17:20:29 +0900

    BaekJoon : 14657번 DFS 문제 풀이 중

--- BaekJoon/DFS/14657_JunOhFinalAce.py
+++ BaekJoon/DFS/14657_JunOhFinalAce.py
@@ -4,6 +4,8 @@
 # 해커 준오가 최대한 많은 문제를 푸는데 걸리는 최소 날짜 수를 출력!
 
 # DFS 방식으로 처리
+# DFS : 깊이 우선 탐색이라고도 부르며, 그래프에서 깊은 부분을 우선적으로 탐색하는 알고리즘
+
 
 # 예제 입력1
 # 6 4  -> 문제의 수 N  하루 풀이 시간 T
@@ -15,5 +17,25 @@
 
 #예제 출력1
 # 4
+
+
+n, t = map(int, input().split())
+
+graph = []
+
+visited = [False] * n
+
+for i in range(n):
+    graph.append(list(map(int,input().split())))
+
+
+def quiz(n,t,v,graph):
+
+    visited[v] = True
+    print(v, end=' ')
+
+    for i in graph[v]:
+        if not visited[i]:
+            quiz(graph, i ,visited)
 
 
