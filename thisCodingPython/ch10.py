8694aaf9043e6c199dfc384cc6819fb7ac5d33f9
nego_home <wnsrn403@naver.com>
2022-04-09 22:58:19 +0900

    codingTestWithPython  - thisCodingPython    : ch10 서로소 집합 10-1 ~ 10-3

--- thisCodingPython/ch10.py
+++ thisCodingPython/ch10.py
@@ -57,3 +57,54 @@ print()
 print('부모 테이블 : ', end='')
 for i in range(1,v+1):
     print(parent[i],end=' ')
+
+
+
+
+# 10-2. 경로 압축기법 소스코드
+def find_parent2(parent,x):
+
+    # 루트노드가 아니라면, 루트노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent2(parent,parent[x])
+    return parent[x]
+
+
+
+
+# 10-3. 개선된 서로소 집합 알고리즘 소스코드
+# 10-2. 경로 압축기법 소스코드 find_parent2 이용하여, 서로소 집합 구하기
+
+# 두 원소가 속한 집합을 합치기
+def union_parent2(parent,a,b):
+    a = find_parent2(parent,a)
+    b = find_parent2(parent,b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int,input().split())
+parent = [0] * (v+1) # 부모 테이블 초기화
+
+# 부모 테이블상에서, 부모를 자기자신으로 초기화
+for i in range(1, v+1):
+    parent[i] = i
+
+# union 연산을 각각 수행
+for i in range(e):
+    a,b = map(int,input().split())
+    union_parent2(parent,a,b)
+
+#각 원소가 속한 집합 출력
+print('각 원소가 속한 집합 : ', end='')
+for i in range(1,v+1):
+    print(find_parent2(parent,i),end=' ')
+
+print()
+
+#부모 테이블 내용 출력
+print('부모 테이블 : ', end='')
+for i in range(1,v+1):
+    print(parent[i],end=' ')
