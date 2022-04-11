4d04d0e959536009636479524dc9e3b9b56af1b6
nego_home <wnsrn403@naver.com>
2022-04-11 22:39:20 +0900

    codingTestWithPython  - thisCodingPython    : ch10 서로소 집합 10-4

--- thisCodingPython/ch10.py
+++ thisCodingPython/ch10.py
@@ -108,3 +108,66 @@ print()
 print('부모 테이블 : ', end='')
 for i in range(1,v+1):
     print(parent[i],end=' ')
+
+
+
+
+
+# 10-4. 서로소 집합을 활용한 사이클 판별 소스코드
+# 입력 예시
+# 3 3
+# 1 2
+# 1 3
+# 2 3
+
+# 출력 예시
+# 사이클이 발생했습니다.
+
+#특정 원소가 속한 집합을 찾기
+def find_parent3(parent,x):
+
+    # 루트노드가 아니라면, 루트노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent3(parent,parent[x])
+    return parent[x]
+
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent3(parent,a,b):
+    a = find_parent3(parent,a)
+    b = find_parent3(parent,b)
+    if a<b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+# 노드의 개수와 간선(union 연산)의 개수 입력받기
+v, e = map(int,input().split())
+parent = [0] * (v+1) # 부모 테이블 초기화
+
+# 부모 테이블상에서, 부모를 자기자신으로 초기화
+for i in range(1, v+1):
+    parent[i] = i
+
+
+cycle = False # 사이클 발생 여부
+
+for i in range(e):
+    a,b = map(int,input().split())
+
+    # 사이클이 발생한 경우 종료
+    if find_parent3(parent,a) == find_parent3(parent,b):
+        cycle = True
+        break
+
+    # 사이클이 발생하지 않았다면 합집합(union) 수행
+    else:
+        union_parent3(parent,a,b)
+
+
+    if cycle:
+        print('사이클이 발생했습니다.')
+    else:
+        print('사이클이 발생하지 않았습니다.')
